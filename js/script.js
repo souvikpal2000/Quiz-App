9a78134907064cac893fdaf805519805e9d35730
Souvik Pal <spal62588@gmail.com>
2021-02-10 11:28:22 +0530

    Fifth Commit

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ async function categoryHeader()
     	}
     	controls = "cat" + category.id;
     	button.setAttribute("aria-controls", controls);
-    	button.innerHTML = `<b class="text-primary">${category.name}</b>`;
+    	button.innerHTML = `<b class="text-secondary">${category.name}</b>`;
     	h2.appendChild(button);
 
     	participantsFunction(category.id);
